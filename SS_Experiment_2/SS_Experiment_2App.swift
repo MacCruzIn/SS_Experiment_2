50738918bd76cbdd1def6d3888f439ccd9e9266d
montyboyer <montyboyer@gmail.com>
2020-07-27 09:23:38 -0700

    Initial Commit

---
+++ SS_Experiment_2/SS_Experiment_2App.swift
@@ -0,0 +1,17 @@
+//
+//  SS_Experiment_2App.swift
+//  SS_Experiment_2
+//
+//  Created by Monty Boyer on 7/27/20.
+//
+
+import SwiftUI
+
+@main
+struct SS_Experiment_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
