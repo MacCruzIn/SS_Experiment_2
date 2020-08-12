4dba16a15b09411d8274a9bef8730e0e3526067d
Ned Hogan <nedhogan@me.com>
2020-08-11 19:45:22 -0700

    Add CocoaPods MS SDK support
    Must use Xcode 12 beta to build and set target for iOS14. Create Pull request so Mont cna confirm we don’t have any issues.
    
    Signed-off-by: Ned Hogan <nedhogan@me.com>

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '14.0'
+use_frameworks!
+target 'SS_Experiment_2' do
+  pod 'AppCenter', '~> 3.3'
+end
---
+++ Pods/Target Support Files/Pods-SS_Experiment_2/Pods-SS_Experiment_2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SS_Experiment_2 : NSObject
+@end
+@implementation PodsDummy_Pods_SS_Experiment_2
+@end
